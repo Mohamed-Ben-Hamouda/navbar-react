b634161ab2fc26f952389e250dd289a1f495cea5
Mohamed-Ben-Hamouda <medbenhamouda81@gmail.com>
2020-02-27 16:13:44 +0100

    Updates

---
+++ precache-manifest.0f3b1b7856d6e4f6677b51b8ef9af0f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5837febce1c8cb67551a396b067bc6c4",
+    "url": "/navbar-react/index.html"
+  },
+  {
+    "revision": "fb97f7d562691e3ecc19",
+    "url": "/navbar-react/static/css/main.64fb472b.chunk.css"
+  },
+  {
+    "revision": "947f6ec76d9cfea8c40b",
+    "url": "/navbar-react/static/js/2.8448a6a8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/navbar-react/static/js/2.8448a6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb97f7d562691e3ecc19",
+    "url": "/navbar-react/static/js/main.012c7541.chunk.js"
+  },
+  {
+    "revision": "6c2fcfbae966424411fa",
+    "url": "/navbar-react/static/js/runtime-main.d41b54cf.js"
+  }
+]);
